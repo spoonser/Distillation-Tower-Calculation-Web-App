8ffc4e5c9ba91c8b16a2c0e4d40644cbbd9f71ae
spencer <spencerwagner0@gmail.com>
2022-08-04 16:38:17 -0700

    Added directories and files for database and webscraping (for database)

---
+++ static/scraper/db_scraper.py
@@ -0,0 +1,8 @@
+# ***************************************************************************
+# * Distillation Column Calculation - Database Web Scraper
+# * Spencer Wagner
+# *
+# * This is a web scraper meant to scrape information from http://www.ddbst.com/vle-databanks.html
+# * the data is not easily accessible on this site and is to be scraped
+# * and placed in a local database for this project
+# ***************************************************************************
